c2e4e190a962613cef403bc41b8b56c828d65879
wolgwang <145632038+wolgwang1729@users.noreply.github.com>
2025-02-01 09:55:31 +0530

    Format VM output with consistent indentation and update test file references

--- Project11/JackCompiler.py
+++ Project11/JackCompiler.py
@@ -282,31 +282,31 @@ class VMWriter:
         self.output_file = output_file
     
     def writePush(self,segment,index):
-        self.output_file.write(f"push {segment} {index}\n")
+        self.output_file.write(f"    push {segment} {index}\n")
 
     def writePop(self,segment,index):
-        self.output_file.write(f"pop {segment} {index}\n")
+        self.output_file.write(f"    pop {segment} {index}\n")
 
     def writeArithmetic(self,command):
-        self.output_file.write(f"{command}\n")
+        self.output_file.write(f"    {command}\n")
     
     def writeLabel(self,label):
         self.output_file.write(f"label {label}\n")
 
     def writeGoto(self,label):
-        self.output_file.write(f"goto {label}\n")
+        self.output_file.write(f"    goto {label}\n")
     
     def writeIf(self,label):
-        self.output_file.write(f"if-goto {label}\n")
+        self.output_file.write(f"    if-goto {label}\n")
 
     def writeCall(self,name,nArgs):
-        self.output_file.write(f"call {name} {nArgs}\n")
+        self.output_file.write(f"    call {name} {nArgs}\n")
 
     def writeFunction(self,name,nLocals):
         self.output_file.write(f"function {name} {nLocals}\n")
 
     def writeReturn(self):
-        self.output_file.write("return\n")
+        self.output_file.write(f"    return\n")
 
     def close(self):
         self.output_file.close()
@@ -928,4 +928,4 @@ class JackCompiler:
             compilationEngine.compileClass()
             output_file.close()
 
-JackCompiler("Project11\TestFiles\ConvertToBin")
+JackCompiler("Project11\TestFiles\seven")
